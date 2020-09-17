78446716b8ae00b23e6ff2d7c9c22accc40049bd
danieldiaz-2502 <daniel_diazvalencia@outlook.com>
2020-09-16 21:49:33 -0500

    Configuracion checkbox

--- app/src/main/java/com/example/myapplication/activity_preguntas2.java
+++ app/src/main/java/com/example/myapplication/activity_preguntas2.java
@@ -6,16 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class activity_preguntas2 extends AppCompatActivity implements View.OnClickListener {
 
     private Button continuarBoton;
+    private CheckBox checkBox1;
+    private CheckBox checkBox2;
+    private CheckBox checkBox3;
+    private CheckBox checkBox4;
+    private CheckBox checkBox5;
+    private CheckBox checkBox6;
+    private CheckBox checkBox7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preguntas2);
         continuarBoton = findViewById(R.id.continuarBoton);
+        checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
+        checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+        checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
+        checkBox4 = (CheckBox) findViewById(R.id.checkBox4);
+        checkBox5 = (CheckBox) findViewById(R.id.checkBox5);
+        checkBox6 = (CheckBox) findViewById(R.id.checkBox4);
+        checkBox7 = (CheckBox) findViewById(R.id.checkBox1);
 
         continuarBoton.setOnClickListener(this);
 
@@ -23,12 +38,14 @@ public class activity_preguntas2 extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        if (checkBox1.isChecked() || checkBox2.isChecked() || checkBox3.isChecked() || checkBox4.isChecked() || checkBox5.isChecked() || checkBox6.isChecked() || checkBox7.isChecked()) {
+            switch (v.getId()) {
 
-            case R.id.registrarBoton:
-                Intent i = new Intent(this, MainActivity.class);
-                startActivity(i);
+                case R.id.continuarBoton:
+                    Intent i = new Intent(this, MainActivity.class);
+                    startActivity(i);
 
+            }
         }
     }
 }
--- app/src/main/java/com/example/myapplication/preguntas1_activity.java
+++ app/src/main/java/com/example/myapplication/preguntas1_activity.java
@@ -6,10 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class preguntas1_activity extends AppCompatActivity implements View.OnClickListener {
 
     private Button continuarBoton;
+    private CheckBox checkBox1;
+    private CheckBox checkBox2;
+    private CheckBox checkBox3;
+    private CheckBox checkBox4;
+    private CheckBox checkBox5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,12 @@ public class preguntas1_activity extends AppCompatActivity implements View.OnCli
         setContentView(R.layout.activity_preguntas1_activity);
 
         continuarBoton = findViewById(R.id.continuarBoton);
+        checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
+        checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+        checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
+        checkBox4 = (CheckBox) findViewById(R.id.checkBox4);
+        checkBox5 = (CheckBox) findViewById(R.id.checkBox5);
+
 
         continuarBoton.setOnClickListener(this);
 
@@ -24,12 +36,15 @@ public class preguntas1_activity extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        //Para que no pase a la siguiente actividad a menos que haya checado por los menos un checkbox
+        if (checkBox1.isChecked() || checkBox2.isChecked() || checkBox3.isChecked() || checkBox4.isChecked() || checkBox5.isChecked()) {
+            switch (v.getId()) {
 
-            case R.id.registrarBoton:
-                Intent i = new Intent(this, activity_preguntas2.class);
-                startActivity(i);
+                case R.id.continuarBoton:
+                    Intent i = new Intent(this, activity_preguntas2.class);
+                    startActivity(i);
 
+            }
         }
     }
 }
